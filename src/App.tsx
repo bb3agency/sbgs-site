b926ee23a0602f04d255f4cd949c9dde9c091a8f
bb3agency <243941306+bb3agency@users.noreply.github.com>
2026-03-30 05:07:20 +0530

    Fix routing: remove redirect, serve Locations at root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Locations from './pages/Locations';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Temporary redirect to locations since homepage doesn't exist yet */}
-        <Route path="/" element={<Navigate to="/locations" replace />} />
+        <Route path="/" element={<Locations />} />
         <Route path="/locations" element={<Locations />} />
       </Routes>
     </BrowserRouter>
